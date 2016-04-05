01016a3c5920e4459d3b6195d17fa5ac35792358
fmamimi <fmamimi@gmail.com>
2016-04-05 11:37:46 -0400

    added some examples for the issue

---
+++ Example-Simplest/js/main.js
@@ -0,0 +1,43 @@
+var color = d3.scale.category10();
+
+var canvas = d3.select("body").append("svg")
+.attr("width",500)
+.attr("height",500);
+
+d3.json("data/data-ar.json", function(data){
+
+	//put data into treemap layout
+	var treemap = d3.layout.treemap()
+				.size([500, 500])
+				.nodes(data);
+				
+	//get cells from the layout
+	var cells = canvas.selectAll(".cell")
+				.data(treemap)
+				.enter()
+				.append("g")
+				.attr("class", "cell");
+				
+	//draw cells
+	cells.append("rect")
+				.attr("x", function (d) {return d.x;})
+				.attr("y", function (d) {return d.y;})
+				.attr("width", function (d) {return d.dx;})
+				.attr("height", function (d) {return d.dy;})
+				.attr("fill", function(d){ return d.children? null : color(d.parent.name);})
+				.attr("stroke","white");
+
+	cells.append("text")
+				.attr("x", function(d){return d.x + d.dx/2;})
+				.attr("y", function(d){return d.y + d.dy/2;})
+				.attr("text-anchor", "middle")
+				.attr("font-size","10px")
+				.text(function(d) {
+					if(d.children)
+						return null
+					else
+					{
+						return ""//d.value + " | " + d.dx + " x "+ d.dy + " = "+d.dx * d.dy;
+					}
+				})
+})
